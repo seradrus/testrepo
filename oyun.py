57a2bb30417c41c7f82597970221ea1234203d5a
seradrus <95140130+seradrus@users.noreply.github.com>
2021-11-30 21:05:15 +0300

    Add files via upload

---
+++ oyun.py
@@ -0,0 +1,17 @@
+print ("sayıyı tahmin edin")    
+      
+print("ilk sayınız 10 ile 20 arasında. haydi tahmin edin: ")
+
+tahmin = input("sayı söyleyin:")
+
+if tahmin == "15":
+    print ("doğru")
+else:
+    print ("yanlış, bilemediniz")
+
+print("ikinci sayınız 10 ile 30 arasıda")
+print("sayı söyleyin")
+if tahmin == "15":
+     print ("doğru")
+else:
+    print ("yanlış")
